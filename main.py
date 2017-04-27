f482e7a5655209d001e549be36e8f2b5458a66ea
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2017-04-27 19:12:25 -0300

    Added new Player Sprite
    -And corrected current frame with right colors;

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from PPlay.sprite import *
 canvas = Window(800,600)
 keyboard = Window.get_keyboard()
 
-player = Sprite("boy.png", 8)
+player = Sprite("boy.png", 24)
 player_x = 20
 player_y = 50
 player_color = "RED"
@@ -33,7 +33,8 @@ player_flag_space = True
 ############
 def on_create():
     player.set_position(player_x, player_y)
-    player.set_total_duration(500)
+    player.set_sequence(0,8)
+    player.set_total_duration(1600)
 
 ############
 ###A Funcao player_move e a responsavel pela movimentacao do personagem.
@@ -66,15 +67,22 @@ def player_change_color():
     if(keyboard.key_pressed("SPACE")):
         if(player_flag_space):
             player_flag_space = False
+            temp_curr_frame = player.get_curr_frame()
 
             if(player_color == "RED"):
                 player_color = "GREEN"
+                player.set_sequence(9,16)
+                player.set_curr_frame(temp_curr_frame + 8)
+
             elif(player_color == "GREEN"):
                 player_color = "BLUE"
+                player.set_sequence(17,24)
+                player.set_curr_frame(temp_curr_frame + 8)
+
             elif(player_color == "BLUE"):
                 player_color = "RED"
-
-            print(player_color)
+                player.set_sequence(0,8)
+                player.set_curr_frame(temp_curr_frame - 16)
     else:
         player_flag_space = True
 
