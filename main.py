2ba6c166dd74e82a17952bd2d7c89b4fb717e013
Thiago do Prado <prado.tps@gmail.com>
2017-06-19 09:50:28 -0300

    Highscore, Button animation, screens
    -Big changes for te project;
    -Thanks for Hugo Pierre!

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ current_button = "PLAY"
 button_play = GameImage("resources/images/Play.png")
 button_play_x = canvas.width / 2 - button_play.width / 2
 button_play_y = canvas.height / 2 + button_play.height / 2
-
-button_options = GameImage("resources/images/Options.png")
-button_options_x = ((canvas.width / 2 - button_options.width / 2) - button_options.width / 2) - 30
-button_options_y = (canvas.height / 2 + button_play.height / 2) + button_options.height + 10
+button_play_sprite = Sprite("resources/images/Play_Sprite.png", 2)
+button_play_sprite.set_total_duration(1000)
 
 button_credits = GameImage("resources/images/Credits.png")
-button_credits_x = ((canvas.width / 2 - button_credits.width / 2) + button_credits.width / 2) + 30
+button_credits_x = canvas.width / 2 - button_credits.width / 2
 button_credits_y = (canvas.height / 2 + button_play.height / 2) + button_credits.height + 10
+button_credits_sprite = Sprite("resources/images/Credits_Button_Sprite.png", 2)
+button_credits_sprite.set_total_duration(1000)
 
 score = 0
 score_streak = 1
@@ -44,13 +44,13 @@ background2 = GameImage("resources/images/Background.png")
 background_x = 0
 background2_x = 800
 
-clouds = GameImage("resources/images/Nuvens.png")
-clouds2 = GameImage("resources/images/Nuvens.png")
+clouds = GameImage("resources/images/Clouds.png")
+clouds2 = GameImage("resources/images/Clouds.png")
 clouds_x = 0
 clouds2_x = 800
 
-bush = GameImage("resources/images/Arbusto.png")
-bush2 = GameImage("resources/images/Arbusto.png")
+bush = GameImage("resources/images/Bush.png")
+bush2 = GameImage("resources/images/Bush.png")
 bush_x = 0
 bush2_x = 800
 
@@ -66,6 +66,15 @@ player_flag_space = True
 enemies = []
 enemies_colors = []
 enemies_flag_create = True
+
+high_score_file = open("resources/files/High_score.txt", "r+")
+high_score = int(high_score_file.readlines()[0])
+high_score_sprite = Sprite("resources/images/High_score.png", 2)
+high_score_sprite.set_total_duration(1000)
+
+credits = Sprite("resources/images/Credits_Sprite.png", 2)
+credits.set_position(0, 0)
+credits.set_total_duration(1000)
 #endregion
 
 #region Functions
@@ -81,15 +90,17 @@ def on_create():
     player.set_total_duration(1600)
 
     button_play.set_position(button_play_x, button_play_y)
-    button_options.set_position(button_options_x, button_options_y)
     button_credits.set_position(button_credits_x, button_credits_y)
+
+    button_play_sprite.set_position(button_play_x, button_play_y)
+    button_credits_sprite.set_position(button_credits_x, button_credits_y)
 
 
 ############
 ###A Funcao on_restart reinicia as variaveis necessarias para o gameplay.
 ############
 def on_restart():
-    global player_x, player_y, player_color, enemies, enemies_colors, background_x, background2_x, clouds_x, clouds2_x, bush_x, bush2_x
+    global player_x, player_y, player_color, enemies, enemies_colors, background_x, background2_x, clouds_x, clouds2_x, bush_x, bush2_x, high_score_file, high_score
 
     player_x = 20
     player_y = 280
@@ -108,10 +119,13 @@ def on_restart():
     bush2_x = 800
 
     player.set_position(player_x, player_y)
-    player.set_sequence(0,8)
+    player.set_sequence(0,4)
 
     background.set_position(background_x, 0)
     background2.set_position(background2_x, 0)
+
+    high_score_file = open("resources/files/High_score.txt", "r+")
+    high_score = int(high_score_file.readlines()[0])
 #endregion
 
 #region Player's Functions
@@ -122,19 +136,19 @@ def player_move():
     global player_x, player_y, player_flag_up, player_flag_down
 
     if(keyboard.key_pressed("DOWN")):
-        if(player_y >= 130 and player_y < 470 and player_flag_down):
+        if(player_y >= 130 and player_y < 430 and player_flag_down):
             player_y += 150
             player_flag_down = False
     else:
         player_flag_down = True
 
     if(keyboard.key_pressed("UP")):
-        if(player_y > 130 and player_y <= 470 and player_flag_up):
+        if(player_y > 130 and player_y <= 430 and player_flag_up):
             player_y -= 150
             player_flag_up = False
     else:
         player_flag_up = True
-
+    
     player.set_position(player_x, player_y)
 
 
@@ -151,12 +165,12 @@ def player_change_color():
 
             if(player_color == "RED"):
                 player_color = "YELLOW"
-                player.set_sequence(5, 8)
+                player.set_sequence(4, 8)
                 player.set_curr_frame(temp_curr_frame + 4)
 
             elif(player_color == "YELLOW"):
                 player_color = "BLUE"
-                player.set_sequence(9, 12)
+                player.set_sequence(8, 12)
                 player.set_curr_frame(temp_curr_frame + 4)
 
             elif(player_color == "BLUE"):
@@ -344,7 +358,7 @@ def background_draw():
 
 #region Score's Functions
 ############
-###A Funcao score_update e responsavel por aumentar os valores de score e score_streak.
+###A Funcao score_add e responsavel por aumentar os valores de score e score_streak.
 ############
 def score_add():
     global score, score_streak
@@ -361,9 +375,15 @@ def score_add():
 ###A Funcao score_update e responsavel por reiniciar os valores de score e score_streak.
 ############
 def score_update():
-    global score, score_streak
+    global score, score_streak, screen
 
     if(screen == "GAMEOVER"):
+        if score > high_score:
+            high_score_file.seek(0, 0)
+            high_score_file.truncate()
+            high_score_file.write(str(score))
+            high_score_file.close()
+            screen = "HIGH_SCORE"
         score = 0
         score_streak = 1
 
@@ -372,10 +392,10 @@ def score_update():
 ###A Funcao score_draw e responsavel por desenhar a pontuacao na tela.
 ############
 def score_draw ():
-    canvas.draw_text("Pontos: %s" % (score), 20, 20, size = 20, color = (0,0,0), font_name = "Arial", bold = True, italic = False)
-
+    canvas.draw_text("High score: %s" % (high_score), 20, 20, size = 20, color = (0,0,0), font_name = "Arial", bold = True, italic = False)
+    canvas.draw_text("Score: %s" % (score), 20, 50, size = 20, color = (0,0,0), font_name = "Arial", bold = True, italic = False)
     if (score != 0):
-        canvas.draw_text("+%s" % (1 * score_streak), 20, 50, size = 20, color = (0,0,0), font_name = "Arial", bold = True, italic = False)
+        canvas.draw_text("+%s" % (1 * score_streak), 20, 80, size = 20, color = (0,0,0), font_name = "Arial", bold = True, italic = False)
 #endregion
 
 #region Buttons' Functions
@@ -383,9 +403,14 @@ def score_draw ():
 ###A Funcao button_draw aglomera todas as funcoes de desenho de todos os botoes.
 ############
 def button_draw():
-    button_play.draw()
-    button_options.draw()
-    button_credits.draw()
+    if(screen == "MENU"):
+        if(current_button == "CREDITS"):
+            button_credits_sprite.draw()
+            button_play.draw()
+
+        elif(current_button == "PLAY"):
+            button_play_sprite.draw()
+            button_credits.draw()
 #endregion
 
 #region Utility Functions
@@ -407,32 +432,32 @@ def navigate():
     global current_button, screen
     if(screen == "MENU"):
         if(keyboard.key_pressed("DOWN") and current_button == "PLAY"):
-            current_button = "OPTIONS"
-        elif(keyboard.key_pressed("RIGHT") and current_button == "OPTIONS"):
             current_button = "CREDITS"
-        elif(keyboard.key_pressed("LEFT") and current_button == "CREDITS"):
-            current_button = "OPTIONS"
-        elif(keyboard.key_pressed("UP") and current_button == "OPTIONS"):
-            current_button = "PLAY"
+
         elif(keyboard.key_pressed("UP") and current_button == "CREDITS"):
             current_button = "PLAY"
 
         if(keyboard.key_pressed("ENTER") or keyboard.key_pressed("SPACE")):
             if(current_button == "PLAY"):
                 screen = "GAMEPLAY"
-            if(current_button == "OPTIONS"):
-                screen = "OPTIONS"
             if(current_button == "CREDITS"):
                 screen = "CREDITS"
 
     if(keyboard.key_pressed("ESC")):
-        if(current_button == "BACK"):
+        if(current_button == "BACK") and (screen != "HIGH_SCORE"):
             screen = "MENU"
             current_button = "PLAY"
 
+        if(current_button == "BACK") and (screen == "HIGH_SCORE"):
+            screen = "GAMEOVER"
+
     if(screen == "GAMEPLAY"):
         current_button = "PLAY"
-    if(screen == "CREDITS" or screen == "OPTIONS"):
+
+    if(screen == "CREDITS"):
+        current_button = "BACK"
+
+    if(screen == "HIGH_SCORE"):
         current_button = "BACK"
 #endregion
 
@@ -450,6 +475,18 @@ def update():
     if(screen == "GAMEPLAY"):
         player_update()
         enemy_update()
+
+    if(screen == "CREDITS"):
+        credits.update()
+
+    if(screen == "HIGH_SCORE"):
+        high_score_sprite.update()
+
+    if(screen == "MENU") and (current_button == "PLAY"):
+        button_play_sprite.update()
+
+    if(screen == "MENU") and (current_button == "CREDITS"):
+        button_credits_sprite.update()
 
     score_update()
 
@@ -470,6 +507,11 @@ def draw():
         player.draw()
         enemy_draw()
 
+    if(screen == "CREDITS"):
+        credits.draw()
+
+    if(screen == "HIGH_SCORE"):
+        high_score_sprite.draw()
     bush_draw()      #O desenho do arbusto esta aqui para sobrepor todas as imagens
 
 
